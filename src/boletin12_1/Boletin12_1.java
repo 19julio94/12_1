3cfc5ea64704362c8152cc1ca7811b0feec2343c
jpatriciodasilva <jpatriciodasilva@atanasoff06.danielcastelao.org>
2015-12-15 08:28:19 +0100

    final

--- src/boletin12_1/Boletin12_1.java
+++ src/boletin12_1/Boletin12_1.java
@@ -1,6 +1,8 @@
 
 package boletin12_1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jpatriciodasilva
@@ -9,7 +11,16 @@ public class Boletin12_1 {
 
    
     public static void main(String[] args) {
+        int x=0;
+        do{
+        Garaxe gar1=new Garaxe();
         
+        gar1.setMatricula(null);
+     
+        gar1.setTempo();
+        gar1.calculo(Float.parseFloat(JOptionPane.showInputDialog("Introduce os cartos")));
+        x++;
+        }while(x<5);
     }
     
 }
--- src/boletin12_1/Garaxe.java
+++ src/boletin12_1/Garaxe.java
@@ -2,86 +2,67 @@ package boletin12_1;
 
 import javax.swing.JOptionPane;
 
-
 public class Garaxe {
+
     int numeroCoches;
     String matricula;
     int tempo;
     float pago;
-    float devolucion;
-    int nPlazas=5;
-    int i=0;
-    public Garaxe(){}
-    
-    public void entrada(int num){
-            numeroCoches=num;
-        boolean vacantes=true;
-        
-        
-         if (numeroCoches>=nPlazas){
-             
-             vacantes=false;
-         JOptionPane.showMessageDialog(null, "COMPLETO");
-                           
-             
-         }
-         else{
-         JOptionPane.showMessageDialog(null, "DISPOÑIBLE");
-         }
-         
-    
-               
-       
-  }
-    
-    public void calculo(float pago){
-        
-        if(tempo>3){
-           devolucion=pago-1.5f+0.2f*tempo-3;
-            
-            
-            
+    float custo;
+
+    int nPlazas = 5;
+    int i = 0;
+
+    public Garaxe() {
+    }
+
+    public void entrada(int num) {
+        numeroCoches = num;
+        boolean vacantes = true;
+
+        if (numeroCoches >= nPlazas) {
+
+            vacantes = false;
+            JOptionPane.showMessageDialog(null, "COMPLETO");
+
+        } else {
+            JOptionPane.showMessageDialog(null, "DISPOÑIBLE");
         }
-        else{
-            devolucion=pago-1.5f;
-            
-            
-         }
-        
-        JOptionPane.showMessageDialog(null, "O cambio é "+devolucion);
-        
-        
-        
-        
-        
-        
+
     }
-    
-    public void setMatricula(String matricula){
-    
-    this.matricula=JOptionPane.showInputDialog("Introduce a matricula");
-    i++;
-    
-    if(i>=5){
-    
-    JOptionPane.showMessageDialog(null, "Parking Completo");
-    
-    
+
+    public void calculo(float pago) {
+
+        if (tempo > 3) {
+            custo = pago - 1.5f + 0.2f * tempo - 3;
+
+        } else {
+            custo = pago - 1.5f;
+
+        }
+
+        JOptionPane.showMessageDialog(null,"A matricula é:"+matricula+"\n"+"O tempo no parking foi:"+tempo+"\n"+"O cambio é " + custo);
+
     }
-    
+
+    public void setMatricula(String matricula) {
+
+        this.matricula = JOptionPane.showInputDialog("Introduce a matricula");
+        i++;
+
+        if (i >= 5) {
+
+            JOptionPane.showMessageDialog(null, "Parking Completo");
+
+        }
+
     }
-    
-    public void setTempo(int tempo){
-     this.tempo=Integer.parseInt(JOptionPane.showInputDialog("Introduce o tempo que estiveches no parking"));
-    
-    
-    
+
+    public void setTempo() {
+        this.tempo = Integer.parseInt(JOptionPane.showInputDialog("Introduce o tempo que estiveches no parking"));
+
     }
-    
-    
-    
-    
-    
-    
-    
+
+   
+
 }
